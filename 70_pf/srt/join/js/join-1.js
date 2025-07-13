c3953cd1cc89c793bd95852524b8a3de023b32c7
jei0530 <sally0261@naver.com>
2025-07-13 21:02:48 +0900

    0713

--- 70_pf/srt/join/js/join-1.js
+++ 70_pf/srt/join/js/join-1.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('cancle').onclick = function() {
-        location.href = '/w0310/70_pf/srt/join/join-1.html';
+        location.href = '/w0310/70_pf/srt/index.html';
     };
 
     document.getElementById('next').onclick = function() {
         const form = document.querySelector('form');
         if (form.checkValidity()) {
-            location.href = '/w0310/70_pf/srt/join/join-3.html';
+            location.href = '/w0310/70_pf/srt/join/join-2.html';
         } else {
             form.reportValidity();
         }
